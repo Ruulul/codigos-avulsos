ef05469dba3ba025c086ffecf9ffc2f14dec719d
Ruulul <vamjunior01@gmail.com>
2022-12-03 02:24:45 -0300

    indent and order

--- advent-of-code/day1-2.zig
+++ advent-of-code/day1-2.zig
@@ -3,9 +3,10 @@ const input = @embedFile("./day1.txt");
 
 pub fn main() void {
     var input_lines = std.mem.split(u8, input, "\n");
+    
     const n = 3;
     var maxs: [n]u32 = .{0}**n;
-    var total:u32 = 0;
+    
     var acc: u32 = 0;
     while (input_lines.next()) |line| {
         if (line.len == 0) {
@@ -20,6 +21,9 @@ pub fn main() void {
         }
         acc += std.fmt.parseInt(u32, line, 0) catch unreachable;
     }
+
+    var total: u32 = 0;
     for (maxs) |max| total += max;
+    
     std.debug.print("{}", .{total});
 }
