f82600af66a5c3171bd53bcac3a06b67ad68ee15
Ruulul <vamjunior01@gmail.com>
2022-12-07 17:52:15 -0300

    new algorithms, benchmark

--- challenges-impulso/maior_palindromo.zig
+++ challenges-impulso/maior_palindromo.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
-const input = "alareviverarara";
-const expected_output = "arevivera";
+const inputs = [_][]const u8{"alareviverarara","ghiabcdefhelloadamhelloabcdefghi"};
 
 fn isPalindrome(string: []const u8) bool {
     const end = string.len / 2;
@@ -10,15 +9,14 @@ fn isPalindrome(string: []const u8) bool {
     } else true;
 }
 
-fn findBiggestPalindrome(string: []const u8) []const u8 {
+fn bruteForce(string: []const u8) []const u8 {
     var index: usize = 0;
     var size: usize = 0;
 
     for (string) |_, start| {
-        var end: usize = start;
-        while (end < string.len) : (end += 1) {
-            const len = end - start;
-            if (isPalindrome(string[start..end]) and len > size) {
+        for (string[start..]) |_, len| {
+            if (len <= size) continue;
+            if (isPalindrome(string[start..][0..len]) and len > size) {
                 size = len;
                 index = start;
             }
@@ -28,12 +26,63 @@ fn findBiggestPalindrome(string: []const u8) []const u8 {
     return string[index..][0..size];
 }
 
+fn dynamicApproach(string: []const u8) []const u8 {
+    var index: usize = 0;
+    var size: usize = 0;
+
+    for (string) |char, i| {
+        var low = if (i == 0) 0 else i - 1;
+        var high = if (i == string.len - 1) i else i + 1;
+
+        while (high < string.len and string[high] == char) high += 1;
+        while (low > 0 and string[low] == char) low -= 1;
+        while (low > 0 and high < string.len and string[low] == string[high]) {
+            low -= 1;
+            high += 1;
+        }
+
+        const len = high - low - 1;
+        if (len > size) {
+            size = len;
+            index = low + 1;
+        }
+    }
+
+    return string[index..][0..size];
+}
+
+fn runNTimes(n: usize, timer: *std.time.Timer, func: anytype, args: anytype) u64 {
+    var acc: u65 = 0;
+    var i: usize = 0;
+
+    timer.reset();
+    while (i < n) : (i += 1) {
+        _ = @call(.{}, func, args);
+        acc += timer.lap();
+    }
+
+    return @truncate(u64, acc/n);
+}
+
 pub fn main() !void {
-    std.log.debug("{s}", .{findBiggestPalindrome(input)});
-    try std.testing.expect(
-        std.mem.eql(u8, 
-            findBiggestPalindrome(input), 
-            expected_output,
-        )
+    var timer = try std.time.Timer.start();
+    const n = 1000;
+
+    for (inputs) |input| {
+    std.log.debug("input: {s}", .{input});
+    std.log.debug("testing brute force:", .{});
+    std.log.debug("average execution time ({} iterations): {}ms. result: {s}", .{
+        n, 
+        runNTimes(n, &timer, bruteForce, .{input}), 
+        bruteForce(input),
+        }
     );
+    
+    std.log.debug("testing dynamic approach:", .{});    std.log.debug("average execution time ({} iterations): {}ms. result: {s}", .{
+        n, 
+        runNTimes(n, &timer, dynamicApproach, .{input}), 
+        dynamicApproach(input),
+        }
+    );
+    }
 }
