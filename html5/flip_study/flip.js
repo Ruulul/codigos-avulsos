31f6066542fda4a28b40583d4ccaefef4de35f6e
Ruulul <vamjunior01@gmail.com>
2023-01-14 15:27:16 -0300

    ensure every FLIP.flip call goes on requestAnimationFrame

--- html5/flip_study/flip.js
+++ html5/flip_study/flip.js
@@ -15,37 +15,39 @@ class FLIP {
         return _ => {
             this.read()
             fn()
-            requestAnimationFrame(_ => this.flip())
+            this.flip()
         }
     }
     static snap(el) {
         const rect = el.getBoundingClientRect()
         return {
             getBoundingClientRect: _ => rect,
-            style: el.style,
+            style: Object.assign({}, el.style),
         }
     }
     static flip(el, options = {}) {
-        const { duration = 200, easing = 'ease-in-out', fill = 'both', iterations = 1, iterationStart = 0 } = options
-        const first_el = el?.first || el
-        const first = first_el.getBoundingClientRect()
-        const f_opacity = first_el.style.opacity || 1
-        const last_el = el?.last || el
-        const last = last_el.getBoundingClientRect()
-        const l_opacity = last_el.style.opacity || 1
+        requestAnimationFrame(_ => {
+            const { duration = 200, easing = 'ease-in-out', fill = 'both', iterations = 1, iterationStart = 0 } = options
+            const first_el = el?.first || el
+            const first = first_el.getBoundingClientRect()
+            const f_opacity = first_el.style.opacity || 1
+            const last_el = el?.last || el
+            const last = last_el.getBoundingClientRect()
+            const l_opacity = last_el.style.opacity || 1
 
-        const i = {
-            top: first.top - last.top,
-            left: first.left - last.left,
-            width: first.width / last.width,
-            height: first.height / last.height
-        }
+            const i = {
+                top: first.top - last.top,
+                left: first.left - last.left,
+                width: first.width / last.width,
+                height: first.height / last.height
+            }
 
-        last_el.animate([
-            { transformOrigin: 'top left', opacity: f_opacity, transform: ` translate(${i.left}px, ${i.top}px)` },
-            { transformOrigin: 'top left', opacity: l_opacity, transform: 'none' }
-        ], {
-            duration, easing, fill, iterations, iterationStart
+            last_el.animate([
+                { transformOrigin: 'top left', opacity: f_opacity, transform: ` translate(${i.left}px, ${i.top}px)` },
+                { transformOrigin: 'top left', opacity: l_opacity, transform: 'none' }
+            ], {
+                duration, easing, fill, iterations, iterationStart
+            })
         })
     }
 }
