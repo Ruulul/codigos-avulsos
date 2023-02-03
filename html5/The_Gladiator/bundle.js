4609ff04af440971899b127aa79c26f436fa0236
Ruulul <vamjunior01@gmail.com>
2023-02-03 18:06:14 -0300

    new build

--- html5/The_Gladiator/bundle.js
+++ html5/The_Gladiator/bundle.js
@@ -1,32 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const { make_listen, generateId } = require("./Component")
-const LabelledTrack = require("./LabelledTrack")
-
-module.exports = function Character(opts = {}, protocol) {
-    const root = document.createElement("div")
-    Object.assign(root.style, {
-        display: 'grid', width: 'fit-content', justifyItems: 'right',
-        ...opts.root_style
-    })
-
-    const dominant_arm = Track("Dominant Arm", 3)
-    const non_dominant_arm = Track("Non Dominant Arm", 2)
-    const body = Track("Body", 4)
-
-    root.append(dominant_arm, non_dominant_arm, body)
-
-    const name = generateId('ui-character')
-    const listen = make_listen({})
-    const notify = protocol ? protocol(listen, name) : undefined
-    root.notify = listen
-
-    return root
-
-    function Track(label, count) {
-        return LabelledTrack({ label, count, ...opts.tracks }, opts.track_protocol)
-    }
-}
-},{"./Component":2,"./LabelledTrack":4}],2:[function(require,module,exports){
 const Component = {
     generateId: (_ => {
         let count = 0
@@ -50,7 +22,7 @@ const Component = {
     },
 }
 module.exports = Component
-},{}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 const { generateId, make_listen } = require("./Component")
 
 module.exports = function Dot(opts = {}, protocol) {
@@ -58,16 +30,16 @@ module.exports = function Dot(opts = {}, protocol) {
     const size = opts.size ?? '2em'
     Object.assign(dot.style, { border: 'solid black', borderRadius: '2em', width: size, height: size, ...opts.style })
 
-    const colors = opts.colors ?? ['transparent', 'black']
-    const labels = opts.labels
-    let marked = opts.initial ?? 0
+    const states = opts.states
+    const colors = opts.colors ?? Array(states.length).fill().map(_ =>  `#${Math.random().toString(16).slice(2, 8)}`)
+    let state = opts.initial ?? 0
     ensureColor()
 
     const name = generateId('ui-dot')
-    const listen = make_listen({ mark, increment, decrement, get: do_protocol })
+    const listen = make_listen({ mark: color(mark), increment: color(increment), decrement: color(decrement), get: do_protocol })
     const notify = protocol ? protocol(listen, name) : undefined
-    dot.onclick = event(increment)
-    dot.oncontextmenu = event(decrement)
+    dot.onclick = event(color(increment))
+    dot.oncontextmenu = event(color(decrement))
 
     dot.notify = listen
 
@@ -81,28 +53,31 @@ module.exports = function Dot(opts = {}, protocol) {
         }
     }
     function do_protocol() {
-        if (notify) notify({ head: [name], type: 'update', data: { marked, label: labels ? labels[marked] : undefined } })
+        if (notify) notify({ head: [name], type: 'update', data: { state: states[state] } })
     }
 
     function mark(msg) {
-        marked = msg.data.value
-        ensureColor()
+        state = msg.data.value
     }
     function increment() {
-        marked = (marked + 1) % colors.length
-        ensureColor()
+        state = (state + 1) % colors.length
     }
     function decrement() {
-        marked--
-        if (marked < 0) marked += colors.length
-        ensureColor()
+        state--
+        if (state < 0) state += colors.length
+    }
+    function color(fn) {
+        return (...args) => {
+            fn(...args)
+            ensureColor()
+        }
     }
     function ensureColor() {
-        dot.style.backgroundColor = colors[marked]
-        if (labels) dot.title = labels[marked]
+        dot.style.backgroundColor = colors[state]
+        dot.title = state[state]
     }
 }
-},{"./Component":2}],4:[function(require,module,exports){
+},{"./Component":1}],3:[function(require,module,exports){
 const Track = require("./Track")
 
 module.exports = function LabelledTrack(opts = {}, protocol) {
@@ -120,19 +95,23 @@ module.exports = function LabelledTrack(opts = {}, protocol) {
 
     return root
 }
-},{"./Track":5}],5:[function(require,module,exports){
+},{"./Track":4}],4:[function(require,module,exports){
 const { generateId, make_listen, make_protocol } = require("./Component")
 const Dot = require("./Dot")
 
 module.exports = function Track(opts = {}, protocol) {
     const name = generateId('ui-track')
-    const listen = make_listen({})
+    const listen = make_listen({ get: notify_state })
     const notify = protocol ? protocol(listen, name) : undefined
 
     const count = opts.count ?? 3
-    const dots = Array(count).fill().map(_ => Dot(opts.dot_opts, make_protocol({
-        update: msg => notify ? notify(msg) : undefined
-    })))
+    const dots_state = new Map()
+    const dots = Array(count).fill().map((_, index) => Dot(opts.dot_opts, make_protocol({
+        update: msg => {
+            dots_state.get(msg.head[0]).state = msg.data.state
+            notify_state()
+        }
+    }, (notify, id) => dots_state.set(id, { notify, index }))))
 
     const track = document.createElement("div")
     Object.assign(track.style, { display: 'flex', flexFlow: 'row', gap: '0.5em', padding: '0.5em', ...opts.style })
@@ -142,36 +121,86 @@ module.exports = function Track(opts = {}, protocol) {
     track.notify = listen
 
     return track
+
+    function notify_state() {
+        notify({ head: [name], type: 'update', data: get_state(dots_state) })
+    }
 }
-},{"./Component":2,"./Dot":3}],6:[function(require,module,exports){
-const Character = require("./Character")
+
+function get_state(state) {
+    const data = {}
+    for (const dot of state.values()) {
+        const state = dot.state
+        if (!data[state]) data[state] = 0
+        data[state]++
+    }
+    return data
+}
+},{"./Component":1,"./Dot":2}],5:[function(require,module,exports){
+const { make_listen, generateId, make_protocol } = require("./Component")
+const LabelledTrack = require("./LabelledTrack")
+
+module.exports = function Tracks(_tracks, opts = {}, protocol) {
+    const root = document.createElement("div")
+    Object.assign(root.style, {
+        display: 'grid', width: 'fit-content', justifyItems: 'right',
+        ...opts.root_style
+    })
+
+    const name = generateId('ui-tracks')
+    const listen = make_listen({ get: notify_state })
+    const notify = protocol ? protocol(listen, name) : undefined
+    root.notify = listen
+
+    const tracks_state = new Map()
+    const tracks = _tracks.map(({ label, count }) => Track(label, count))
+
+    root.append(...tracks)
+
+    return root
+
+    function Track(label, count) {
+        return LabelledTrack({ label, count, ...opts.tracks }, make_protocol({
+            update(msg) {
+                tracks_state.get(msg.head[0]).state = msg.data
+                notify_state()
+            }
+        }, (notify, id) => tracks_state.set(id, { notify, label })))
+    }
+
+    function notify_state() {
+        notify({ head: [name], type: 'update', data: Object.fromEntries(tracks_state.entries()) })
+    }
+}
+},{"./Component":1,"./LabelledTrack":3}],6:[function(require,module,exports){
+const Tracks = require("./Tracks")
 const { make_protocol } = require("./Component")
 
 const root = document.body
-const logDots = function () {
-    const count = {}
-    last = {}
-    return function listen(msg) {
-        let { label } = msg.data
-        let [from] = msg.head
-        if (!count[label]) count[label] = 0
-        count[label]++
-        if (from in last) count[last[from]]--
-        if (!count[last[from]]) count[last[from]] = undefined
-        if (!count[label]) count[label] = undefined
-        last[from] = label
-        console.log(JSON.stringify(count))
-    }
-}
-const char = Character({
+const char_state = document.createElement("ul")
+const char = Tracks([{ label: "Dominant Arm", count: 3 }, { label: "Non Dominant Arm", count: 2 }, { label: "Body", count: 4 }], {
     tracks: {
         dot_opts: {
+            states: ['Vigor', 'Empty', 'Fatigue', 'Wound'],
             colors: ['green', 'white', 'orange', 'red'],
-            labels: ['Vigor', 'Empty', 'Fatigue', 'Wound'],
         }
     },
-    track_protocol: make_protocol({ update: logDots() })
-})
+}, make_protocol({
+    update: msg =>
+        char_state.innerHTML = `
+            ${Object
+            .entries(msg.data)
+            .map(([key, { label, state }]) =>
+                `<li title="track ${key}">${label}: <ul>${Object
+                    .entries(state)
+                    .map(([key, value]) =>
+                        `<li>${key}: ${value}</li>`
+                    ).join('')
+                }</ul></li>`)
+            .join('')
+        }
+     `
+}))
 
-root.append(char)
-},{"./Character":1,"./Component":2}]},{},[6]);
+root.append(char, char_state)
+},{"./Component":1,"./Tracks":5}]},{},[6]);
